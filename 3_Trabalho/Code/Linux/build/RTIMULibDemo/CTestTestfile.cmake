dc13b3c7fcad3fdccefa773c608e1ac5756e8c8c
arthuevangelista <arthuevangelista@gmail.com>
2018-10-27 12:07:42 +0300

    deleted

--- 3_Trabalho/Code/Linux/build/RTIMULibDemo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/RTIMULib2/Linux/RTIMULibDemo
-# Build directory: /home/pi/RTIMULib2/Linux/build/RTIMULibDemo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- 3_Trabalho/Code/Linux/build/RTIMULibDemoGL/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/RTIMULib2/Linux/RTIMULibDemoGL
-# Build directory: /home/pi/RTIMULib2/Linux/build/RTIMULibDemoGL
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
