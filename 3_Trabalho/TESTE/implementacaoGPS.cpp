e711e47090fb495e1dc291cfe558cded80b4c46a
Arthur Evangelista <arthuevangelista@gmail.com>
2019-06-10 04:31:32 -0300

    Add files via upload

--- 3_Trabalho/TESTE/implementacaoGPS.cpp
+++ 3_Trabalho/TESTE/implementacaoGPS.cpp
@@ -18,33 +18,30 @@ void initGPS(gps_data_t** dataGPS){
   int rc;
 
   fprintf(stderr, "Inicializando GPS ...\n");
-  if((rc = gps_open(source.server, source.port, **dataGPS)) == -1){
+  if((rc = gps_open(GPSD_SHARED_MEMORY, 0, **dataGPS)) == -1){
     fprintf(stderr, "Erro na inicialização do GPS na linha # %d\nRazão do erro: %s\n", __LINE__, gps_errstr(rc));
     exit(EXIT_FAILURE);
   }
-  gps_stream(**dataGPS, WATCH_ENABLE | WATCH_JSON, NULL);
 } // FIM DA FUNÇÃO initGPS
 
 // =======================================================================
 // Função de leitura dos dados do GPS
 // =======================================================================
 void leituraGPS(gps_data_t** dataGPS){
-  /* Espera 2 segundos para receber os dados do GPS */
-  if (gps_waiting (**dataGPS, 2000000)) {
+  // será usada shared memory
       if ((rc = gps_read(**dataGPS)) == -1) {
           fprintf(stderr, "Erro ao realizar a leitura do GPS na linha # %d. Código do erro: %s\n",__LINE__, gps_errstr(rc));
-      } else {
+      }else{
           if (((*dataGPS)->status == STATUS_FIX) &&
               ((*dataGPS)->fix.mode == MODE_2D || (*dataGPS)->fix.mode == MODE_3D) &&
               !isnan((*dataGPS)->fix.latitude) &&
               !isnan((*dataGPS)->fix.longitude)) {
                   // Print para fins de testes. Esta linha deve ser comentada!
                   //fprintf(stderr, "\nLatitude: %f\nLongitude: %f\nVelocidade: %f\nMarca de Tempo: %lf\n", (*dataGPS)->fix.latitude, (*dataGPS)->fix.longitude, (*dataGPS)->fix.speed, (*dataGPS)->fix.time);
-          } else {
+          }else{
               fprintf(stderr, "Sem dados do GPS disponíveis!\n");
           }
-      }
-  } // FIM DO IF DE ESPERA ENTRE LEITURAS
+      } // FIM DO IF DE ESPERA ENTRE LEITURAS
 } // FIM DA FUNÇÃO leituraGPS
 
 // =======================================================================
