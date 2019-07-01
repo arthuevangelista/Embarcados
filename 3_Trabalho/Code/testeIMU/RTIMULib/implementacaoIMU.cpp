4b7056007435860ea49e79ca0be06d57674105f6
Arthur Evangelista <arthuevangelista@gmail.com>
2019-07-01 02:27:35 -0300

    Apenas 2 IMUs
    Alteração realizada para testes com apenas duas unidades IMU referentes à asa

--- 3_Trabalho/Code/testeIMU/RTIMULib/implementacaoIMU.cpp
+++ 3_Trabalho/Code/testeIMU/RTIMULib/implementacaoIMU.cpp
@@ -46,7 +46,7 @@ RTIMU* initIMU(int i){
           buzzerTone('X', 100);
           break;
         case 1:
-          settings = new RTIMUSettings("/home/pi/Embarcados/3_Trabalho/Code/testeIMU/initFiles/MPU6050_1/", "RTIMULib");
+          settings = new RTIMUSettings("/home/pi/Embarcados/3_Trabalho/Code/testeIMU/initFiles/MPU6050_1", "RTIMULib");
 
           imu = RTIMU::createIMU(settings);
 
@@ -63,24 +63,24 @@ RTIMU* initIMU(int i){
           buzzerTone('D', 100);
           buzzerTone('X', 100);
           break;
-        case 2:
-          settings = new RTIMUSettings("/home/pi/Embarcados/3_Trabalho/Code/testeIMU/initFiles/MPU9250_2/", "RTIMULib");
-
-          imu = RTIMU::createIMU(settings);
-
-          if((imu == NULL)||(imu->IMUType() == RTIMU_TYPE_NULL)){
-            printf("Unidade IMU [%d] não identificado na porta SPI.\n", i);
-            exit(1);
-          }
-
-          imu->IMUInit();
-          imu->setSlerpPower(0.02);
-          imu->setGyroEnable(true);
-          imu->setAccelEnable(true);
-          imu->setCompassEnable(true);
-          buzzerTone('A', 100);
-          buzzerTone('X', 100);
-          break;
+        // case 2:
+        //   settings = new RTIMUSettings("/home/pi/Embarcados/3_Trabalho/Code/testeIMU/initFiles/MPU9250_2", "RTIMULib");
+        //
+        //   imu = RTIMU::createIMU(settings);
+        //
+        //   if((imu == NULL)||(imu->IMUType() == RTIMU_TYPE_NULL)){
+        //     printf("Unidade IMU [%d] não identificado na porta SPI.\n", i);
+        //     exit(1);
+        //   }
+        //
+        //   imu->IMUInit();
+        //   imu->setSlerpPower(0.02);
+        //   imu->setGyroEnable(true);
+        //   imu->setAccelEnable(true);
+        //   imu->setCompassEnable(true);
+        //   buzzerTone('A', 100);
+        //   buzzerTone('X', 100);
+        //   break;
         default:
             printf("Nenhuma unidade IMU identificada!\n");
             exit(1);
--- 3_Trabalho/Code/testeIMU/testeIMU.cpp
+++ 3_Trabalho/Code/testeIMU/testeIMU.cpp
@@ -39,26 +39,22 @@ typedef struct torsion{
 volatile bend anguloDeFlexao;
 volatile torsion anguloDeTorcao;
 
-// =======================================================================
-// thread para processamento dos dados na struct
-// =======================================================================
 void* procDadosDir(void* unused){
   imuDataAngulo* imu_struct;
-  imu_struct = (imuDataAngulo*)malloc(sizeof(imuDataAngulo)*3);
+  imu_struct = (imuDataAngulo*)malloc(sizeof(imuDataAngulo)*2);
+  //imu_struct = (imuDataAngulo*)malloc(sizeof(imuDataAngulo)*3);
   imu_struct = (imuDataAngulo *) unused;
 
   double pitch0 = imu_struct->pitch;
   double roll0 = imu_struct->roll;
 
   imu_struct++;
-  imu_struct++;
+  //imu_struct++;
 
   double pitch2 = imu_struct->pitch;
   double roll2 = imu_struct->roll;
 
-  // Calculo do ângulo de Flexão
   anguloDeFlexao.meiaAsaDireita = copysign((pitch0 - std::abs(pitch2)), pitch0);
-  // Calculo do ângulo de Torção
   anguloDeTorcao.meiaAsaDireita = copysign((roll0 - std::abs(roll2)), roll0);
 
   return NULL;
@@ -66,7 +62,8 @@ void* procDadosDir(void* unused){
 
 void* procDadosEsq(void* unused){
   imuDataAngulo* imu_struct;
-  imu_struct = (imuDataAngulo*)malloc(sizeof(imuDataAngulo)*3);
+  imu_struct = (imuDataAngulo*)malloc(sizeof(imuDataAngulo)*2);
+  //imu_struct = (imuDataAngulo*)malloc(sizeof(imuDataAngulo)*3);
   imu_struct = (imuDataAngulo *) unused;
 
   imu_struct++;
@@ -74,48 +71,49 @@ void* procDadosEsq(void* unused){
   double pitch1 = imu_struct->pitch;
   double roll1 = imu_struct->roll;
 
-  imu_struct++;
+  imu_struct--;
+  //imu_struct++;
   double pitch2 = imu_struct->pitch;
   double roll2 = imu_struct->roll;
 
-  // Calculo do ângulo de Flexão
   anguloDeFlexao.meiaAsaEsquerda = copysign((pitch1 - std::abs(pitch2)), pitch1);
-  // Calculo do ângulo de Torção
   anguloDeTorcao.meiaAsaEsquerda = copysign((roll1 - std::abs(roll2)), roll1);
 
   return NULL;
 } // FIM DA THREAD procDadosEsq
 
 int main(){
-  RTIMU *imu[2];
+  RTIMU *imu[1];
+  // RTIMU *imu[2];
 
   int contadorIMU = 0;
 
   pthread_t processamentoDireita;
 	pthread_t processamentoEsquerda;
 
-  // imu_struct[0] e imu_struct[1] são para MPU-6050
-  // imu_struct[2] para MPU-9250
   imuDataAngulo* imu_struct;
-  imu_struct = (imuDataAngulo*)malloc(sizeof(imuDataAngulo)*3);
+  imu_struct = (imuDataAngulo*)malloc(sizeof(imuDataAngulo)*2);
+  //imu_struct = (imuDataAngulo*)malloc(sizeof(imuDataAngulo)*3);
 
   wiringPiSetup();
   buzzerInit();
 
-  // Botão de controle do loop infinito
   pinMode(CONTROL_BUTTON_PIN, INPUT);
 
-  for (contadorIMU = 0; contadorIMU < 3; contadorIMU++) {
+  for (contadorIMU = 0; contadorIMU < 2; contadorIMU++) {
+    //for (contadorIMU = 0; contadorIMU < 3; contadorIMU++) {
     imu[contadorIMU] = initIMU(contadorIMU);
   }
 
-  while (digitalRead(CONTROL_BUTTON_PIN)){
-    for (contadorIMU = 0; contadorIMU < 3; contadorIMU++) {
-      imu_struct += contadorIMU;
+  while (1){
+  //while (digitalRead(CONTROL_BUTTON_PIN)){
+    for (contadorIMU = 0; contadorIMU < 2; contadorIMU++) {
+      // for (contadorIMU = 0; contadorIMU < 3; contadorIMU++) {
       leituraIMU(imu[contadorIMU], imu_struct);
+      imu_struct ++;
     }
 
-  imu_struct -= 2;
+    imu_struct -= 2;
 
     if( pthread_create (&processamentoDireita, NULL, &procDadosDir, (void*) imu_struct) != 0){
       fprintf(stderr, "Erro na inicialização da thread procDadosDir na linha # %d\n", __LINE__);
